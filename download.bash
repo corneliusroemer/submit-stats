2acbad379506b3a476d310f30dcbc36b013290df
Cornelius Roemer <cornelius.roemer@gmail.com>
2021-09-30 21:46:38 +0200

    change: download zst, update data to 2021-09-30

--- download.bash
+++ download.bash
@@ -1,4 +1,5 @@
 set -x
 set -v
-scp worker01.scicore.unibas.ch:~/ncov-simple/archive/pre-processed/metadata.tsv .
+scp worker01.scicore.unibas.ch:~/ncov-simple/archive/pre-processed/metadata.tsv.zst .
+zstd -d metadata.tsv.zst >metadata.tsv
 tsv-filter -H --str-eq country:Switzerland metadata.tsv | tsv-select -H -f strain,date,date_submitted,submitting_lab > meta_switzerland.tsv
