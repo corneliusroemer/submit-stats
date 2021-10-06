a57c851938316ea6ef4c53f1af5eee467f120833
Cornelius Roemer <cornelius.roemer@gmail.com>
2021-10-06 19:50:08 +0200

    feat: put conda activate and python3 analysis.py in download.bash script

--- download.bash
+++ download.bash
@@ -1,5 +1,6 @@
 set -x
 set -v
-scp worker01.scicore.unibas.ch:~/ncov-simple/archive/pre-processed/metadata.tsv.zst .
-zstd -d metadata.tsv.zst >metadata.tsv
+scp -C worker01.scicore.unibas.ch:~/ncov-simple/archive/pre-processed/metadata.tsv .
+conda activate nextstrain
 tsv-filter -H --str-eq country:Switzerland metadata.tsv | tsv-select -H -f strain,date,date_submitted,submitting_lab > meta_switzerland.tsv
+python3 analysis.py
