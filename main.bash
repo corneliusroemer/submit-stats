f765b615efc7b79563dac08e70758fc6ef375f99
Cornelius Roemer <cornelius.roemer@gmail.com>
2021-10-14 18:21:13 +0200

    feat: add automatic generation of most recent weeks filter

--- main.bash
+++ main.bash
@@ -4,3 +4,4 @@ scp -C worker01.scicore.unibas.ch:~/ncov-simple/archive/pre-processed/metadata.t
 conda activate nextstrain
 tsv-filter -H --str-eq country:Switzerland metadata.tsv | tsv-select -H -f strain,date,date_submitted,submitting_lab > meta_switzerland.tsv
 python3 analysis.py
+tsv-filter -H --regex '2:2021-(39|4)' days_diff_by_submitting_lab_and_week.tsv >days_diff_most_recent_weeks.tsv
